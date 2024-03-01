f5ef487fc3d4b4f871e5798d12537ffe1fa75585
rohit <rohit.gupta@neosoftmail.com>
2024-03-01 11:15:19 +0530

    created input parser and validator

---
+++ src/lib/utils.ts
@@ -0,0 +1,26 @@
+export const parseInput = (input: string): (number | null)[] => {
+  const arr: any[] = input.split(",");
+  for (let i = 0; i < arr.length; i++) {
+    const val = arr[i];
+    if (val.length == 0 || Number.isNaN(val)) {
+      arr[i] = null;
+    } else {
+      arr[i] = Number(val);
+    }
+  }
+  return arr;
+};
+
+export const isValidInput = (input: string): boolean => {
+  // remove extra space
+  const trimmedInput = input.trim();
+
+  // split the values
+  const arr = trimmedInput.split(",");
+
+  // root value must not be empty or null
+  const rootVal = arr[0];
+  if (rootVal.length == 0 || Number.isNaN(arr[0])) return false;
+
+  return true;
+};
